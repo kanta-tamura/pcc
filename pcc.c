df94b9912838f760737c410398701d305657f762
Kanta Tamura <kanta03085@gmail.com>
2021-01-02 21:41:17 +0900

    抽象構文木のノードを定義

--- pcc.c
+++ pcc.c
@@ -124,6 +124,26 @@ Token* tokenize() {
 	return head.next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+	ND_ADD,	// +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+	NodeKind kind;	// ノードの型
+	Node* lhs;		// 左辺
+	Node* rhs;		// 右辺
+	int val;		// kindがND_NUMの場合のみ使う
+};
+
+
 int main(int argc, char** argv) {
 	if (argc != 2) {
 		fprintf(stderr, "引数の個数が正しくありません\n");
